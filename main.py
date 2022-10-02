0c1e183ade929493863059a540009ee6a20ab133
yamatohagi <la.luce.ymt0326@gmail.com>
2022-10-02 18:43:09 +0900

    formatter

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 class AppiumTest(unittest.TestCase):
     def setUp(self):
         options = {
@@ -21,73 +22,70 @@ class AppiumTest(unittest.TestCase):
             "automationName": "XCUITest",
             "startIWDP": "true",
             "showXcodeLog": "true",
-            "bundleId": "com.yudo.SaitouSan", # ここを編集
+            "bundleId": "com.yudo.SaitouSan",  # ここを編集
             "udid": "00008101-001254320191001E",
             "xcodeOrgId": "CUYU636622",
             "xcodeSigningId": "iPhone Developer",
-            "newCommandTimeout": 3600
+            "newCommandTimeout": 3600,
         }
 
-        protocol = 'http'
-        host = '0.0.0.0'
-        ipaddress = '0.0.0.0'
-        port = '4723'
-        path = '/wd/hub'
-        self.driver = webdriver.Remote(f'{protocol}://{ipaddress}:{port}{path}', options)
-
+        protocol = "http"
+        host = "0.0.0.0"
+        ipaddress = "0.0.0.0"
+        port = "4723"
+        path = "/wd/hub"
+        self.driver = webdriver.Remote(
+            f"{protocol}://{ipaddress}:{port}{path}", options
+        )
 
     def tearDown(self):
         self.driver.quit()
 
     def test_textfield(self):
-        #トップ画面
-        news_el = self.driver.find_element("accessibility id","bt news off")
+        # トップ画面
+        news_el = self.driver.find_element("accessibility id", "bt news off")
         if news_el:
-            TouchAction(self.driver).tap(None, 200, 380, 1).perform()# 斉藤さんと話すボタン
+            TouchAction(self.driver).tap(None, 200, 380, 1).perform()  # 斉藤さんと話すボタン
             # 電話とカメラ封印電話を選択するところ
-            camera_off_button_el = self.driver.find_element("accessibility id","bt talk disable camera off")
-            TouchAction(self.driver).tap(camera_off_button_el,1).perform()
+            camera_off_button_el = self.driver.find_element(
+                "accessibility id", "bt talk disable camera off"
+            )
+            TouchAction(self.driver).tap(camera_off_button_el, 1).perform()
 
             i = 0
 
             while i < 1000:
-                WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.ID, "相手にコールして通話しよう♪")))
-
+                WebDriverWait(self.driver, 30).until(
+                    EC.presence_of_element_located((By.ID, "相手にコールして通話しよう♪"))
+                )
 
                 try:
-                    self.driver.find_element("accessibility id","通話が開始できませんでした")
-                    TouchAction(self.driver).tap(None, 250, 460, 1).perform()# もう一回
+                    self.driver.find_element("accessibility id", "通話が開始できませんでした")
+                    TouchAction(self.driver).tap(None, 250, 460, 1).perform()  # もう一回
                 except:
                     try:
-                        self.driver.find_element("accessibility id","男")
-                        TouchAction(self.driver).tap(None, 100, 360, 1).perform()# 別の人と話す
-                        TouchAction(self.driver).tap(None, 250, 460, 1).perform()# もう一回
+                        self.driver.find_element("accessibility id", "男")
+                        TouchAction(self.driver).tap(
+                            None, 100, 360, 1
+                        ).perform()  # 別の人と話す
+                        TouchAction(self.driver).tap(
+                            None, 250, 460, 1
+                        ).perform()  # もう一回
                     except:
-                        TouchAction(self.driver).tap(None, 270, 360, 1).perform()# 通話する
-                        TouchAction(self.driver).tap(None, 250, 460, 1).perform()# もう一回
-
+                        TouchAction(self.driver).tap(
+                            None, 270, 360, 1
+                        ).perform()  # 通話する
+                        TouchAction(self.driver).tap(
+                            None, 250, 460, 1
+                        ).perform()  # もう一回
 
             print(i)
             i += 1
 
-
         sleep(100000)
 
 
-        # 値を取得
-        # value = element.get_attribute('value')
-        # self.assertEqual(value, None)
-
-        # テキストを入力
-        # word = "Hello, World!"
-        # element.send_keys(word)
-
-        # 値を取得
-        # value = element.get_attribute('value')
-        # self.assertEqual(value, word)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # テストを実行
     suite = unittest.TestLoader().loadTestsFromTestCase(AppiumTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
-
